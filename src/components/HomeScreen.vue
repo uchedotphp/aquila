fa4601c4f8bfee1274cdee5a54ed83605c2b066e
Nwulu Uchechulwu Prince <uchedotphp@gmail.com>
2021-01-08 09:01:46 +0100

    done with designing the pages index

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <main class="wrapper">
+    <p>aquila aveion</p>
+    <div class="img-holder">
+      <img
+        src="./../assets/images/presents.png"
+        alt="model squeezing an orage"
+      />
+    </div>
+  </main>
 </template>
 
 <script>
@@ -9,4 +17,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  display: grid;
+  justify-content: center;
+
+  p {
+    text-align: center;
+    word-spacing: 1em;
+    margin-bottom: 1.5em;
+  }
+
+  .img-holder {
+    text-align: center;
+    img {
+      // box-shadow: 0px 0px 20px 0px black;
+      //   box-shadow: 0 10px 15px -3px black;
+      //   box-shadow: 0 4px 6px -2px black;
+      box-shadow: -10px 5px 50px black;
+    }
+  }
+}
 </style>
