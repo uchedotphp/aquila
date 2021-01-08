c3879659acd38c1785b1e17e16cf9e9b04ee9c6e
Nwulu Uchechulwu Prince <uchedotphp@gmail.com>
2021-01-08 10:54:08 +0100

    done designing navigation and socilas link

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -7,12 +7,23 @@
         alt="model squeezing an orage"
       />
     </div>
+    <div class="socials">
+      <img src="./../assets/images/logo-instagram.svg" alt="instagram" />
+      <img src="./../assets/images/logo-twitter.svg" alt="twitter" />
+      <img src="./../assets/images/logo-snapchat.svg" alt="" srcset="">
+      <img src="./../assets/images/mail-outline (1).svg" alt="mail" />
+    </div>
+    <NavBar />
   </main>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
 export default {
   name: "HomeScreen",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
@@ -29,12 +40,28 @@ export default {
 
   .img-holder {
     text-align: center;
+    margin-bottom: 2em;
     img {
       // box-shadow: 0px 0px 20px 0px black;
       //   box-shadow: 0 10px 15px -3px black;
       //   box-shadow: 0 4px 6px -2px black;
       box-shadow: -10px 5px 50px black;
     }
+  }
+}
+
+.socials {
+  display: grid;
+  place-content: center;
+  height: 100%;
+  position: fixed;
+  right: 1em;
+  img {
+    height: 3em;
+    background: #fff;
+    border-radius: 5px;
+    margin-bottom: 3em;
+    padding: .5em;
   }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav>
+    <a href="#">home</a>
+    <a href="#">about</a>
+    <img src="./../assets/images/face-model-two.png" alt="face model" />
+    <a href="#">gallery</a>
+    <a href="#">contact</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavvBar",
+};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+//   background: chartreuse;
+
+  a {
+    display: inline-block;
+    text-decoration: none;
+    margin-right: 3em;
+    text-transform: uppercase;
+  }
+
+  a:last-child {
+    margin-right: 0;
+  }
+
+  img {
+    height: 12em;
+    clip-path: circle(40%);
+    margin-right: 0;
+    // margin: 0 5em;
+  }
+}
+</style>
